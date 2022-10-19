06cbf60238d10d8b640eb4267f2bd6b8eb1364db
gabrielpc4 <s.gabrielpc@gmail.com>
2022-10-19 15:39:55 -0300

    Simplify MovieslistViewModelTests

--- MoviesAppTests/Data/Mocks/MockMoviesRepository.swift
+++ MoviesAppTests/Data/Mocks/MockMoviesRepository.swift
@@ -1,9 +1,12 @@
 @testable import MoviesApp
+import XCTest
 
 class MockMoviesRepository: MoviesRepositoryProtocol {
     var mockMovies: [Movie] = []
+    var expectation: XCTestExpectation?
     
     func getMovies(pagina: Int, completionHandler: @escaping ([Movie]) -> Void) {
+        expectation?.fulfill()
         completionHandler(mockMovies)
     }
 }
